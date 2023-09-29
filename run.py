45c34a906379e29172359911af3b52eb08769f76
adrianskelton <>
2023-09-29 08:17:00 +0000

    add calculated surplus data to surplus data sheet

--- run.py
+++ run.py
@@ -17,18 +17,21 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 def get_sales_data():
     """
     Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
-    while True: 
+    while True:
         print("Please enter sales data from the last market.")
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
+
         sales_data = data_str.split(",")
-        
 
         if validate_data(sales_data):
-            print("data is valid")
+            print("Data is valid!")
             break
 
     return sales_data
@@ -52,14 +55,26 @@ def validate_data(values):
 
     return True
 
+
 def update_sales_worksheet(data):
-    """"
-    Update sales worksheet, add new row with the list data provided.
     """
-    print("updating sales worksheet...\n")
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
-    print("sales worksheet updated successfully.\n")
+    print("Sales worksheet updated successfully.\n")
+
+
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, add new row with the list data provided
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully.\n")
+
 
 def calculate_surplus_data(sales_row):
     """
@@ -69,10 +84,17 @@ def calculate_surplus_data(sales_row):
     - Positive surplus indicates waste
     - Negative surplus indicates extra made when stock was sold out.
     """
-    print("calculating surplus data...\n")
+    print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
-    stock_row = stock[-1] 
-    print(stock_row)
+    stock_row = stock[-1]
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
+
 
 def main():
     """
@@ -81,8 +103,9 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
-    
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(new_surplus_data)
 
-print("Welcome to love sandwiches data automation")
+
+print("Welcome to Love Sandwiches Data Automation")
 main()
